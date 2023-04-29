d0c5c20b1a72caabd8f28c1cbcb48009518e92e1
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2023-04-28 21:16:15 -0400

    debug test

--- src/packets/builders/query.ts
+++ src/packets/builders/query.ts
@@ -3,7 +3,8 @@ import { BufferWriter, encode } from "../../buffer.ts";
 
 /** @ignore */
 export function buildQuery(sql: string, params: any[] = []): Uint8Array {
-  const data = encode(replaceParams(sql, params));
+  const queryStr = replaceParams(sql, params);
+  const data = encode(queryStr);
   const writer = new BufferWriter(new Uint8Array(data.length + 1));
   writer.write(0x03);
   writer.writeBuffer(data);
