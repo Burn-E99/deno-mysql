870119756cb26c9f90826bc787fe2ef51702238b
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2023-04-28 21:17:32 -0400

    actual debug test

--- src/packets/builders/query.ts
+++ src/packets/builders/query.ts
@@ -4,6 +4,7 @@ import { BufferWriter, encode } from "../../buffer.ts";
 /** @ignore */
 export function buildQuery(sql: string, params: any[] = []): Uint8Array {
   const queryStr = replaceParams(sql, params);
+  console.log(queryStr)
   const data = encode(queryStr);
   const writer = new BufferWriter(new Uint8Array(data.length + 1));
   writer.write(0x03);
